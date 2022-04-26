454d316358c020a1b25405b433571d1d9cf87273
KorzikAlex <lumic.alex@gmail.com>
2022-04-26 15:46:02 +0300

    Main program

--- main_app.py
+++ main_app.py
@@ -4,7 +4,8 @@ from data import db_session
 from data.users import User
 from data.apps import App
 import os
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, login_required, logout_user
+from forms.add_app import add_app
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -20,35 +21,78 @@ def load_user(user_id):
     return user
 
 
-def get_info():
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+def get_info(key=''):
     db_sess = db_session.create_session()
     table = {'apps': []}
-    name_apps = [i.name_app.capitalize() for i in db_sess.query(App).all()]
-    descriptions = [i.description.capitalize() for i in db_sess.query(App).all()]
-    urls = [f'../app/{i.name_app}' for i in db_sess.query(App).all()]
-    download_urls = [i.link for i in db_sess.query(App).all()]
-    icons = [f'../{i.icon}' for i in db_sess.query(App).all()]
-    for i in range(len(name_apps)):
-        table['apps'].append({'name': name_apps[i], 'description': descriptions[i], 'url': urls[i], 'icon': icons[i],
-                              'link': download_urls[i]})
+    if key:
+        name_apps = [i.name_app for i in db_sess.query(App).filter(App.name_app.like(f'%{key}%'))]
+        descriptions = [i.description for i in db_sess.query(App).filter(App.name_app.like(f'%{key}%'))]
+        companies = [i.company for i in db_sess.query(App).filter(App.name_app.like(f'%{key}%'))]
+        pages_companies = [i.page_company for i in db_sess.query(App).filter(App.name_app.like(f'%{key}%'))]
+        urls = [f'../app/{i.name_app}' for i in db_sess.query(App).filter(App.name_app.like(f'%{key}%'))]
+        download_urls = [i.link for i in db_sess.query(App).filter(App.name_app.like(f'%{key}%'))]
+        icons = [f'../{i.icon}' for i in db_sess.query(App).filter(App.name_app.like(f'%{key}%'))]
+        for i in range(len(name_apps)):
+            table['apps'].append(
+                {'name': name_apps[i], 'description': descriptions[i], 'url': urls[i], 'icon': icons[i],
+                 'link': download_urls[i], 'company': companies[i], 'page_company': pages_companies[i]})
+    else:
+        name_apps = [i.name_app for i in db_sess.query(App).all()]
+        descriptions = [i.description for i in db_sess.query(App).all()]
+        pages_companies = [i.page_company for i in db_sess.query(App).all()]
+        companies = [i.company for i in db_sess.query(App).all()]
+        urls = [f'../app/{i.name_app}' for i in db_sess.query(App).all()]
+        download_urls = [i.link for i in db_sess.query(App).all()]
+        icons = [f'../{i.icon}' for i in db_sess.query(App).all()]
+        for i in range(len(name_apps)):
+            table['apps'].append(
+                {'name': name_apps[i], 'description': descriptions[i], 'url': urls[i], 'icon': icons[i],
+                 'link': download_urls[i], 'company': companies[i], 'page_company': pages_companies[i]})
     return table
 
 
-@app.route('/')
-@app.route('/home')
+@app.route('/search/<search>', methods=['GET', 'POST'])
+def app_search(search):
+    if request.method == 'POST':
+        search = request.form.get('search')
+        if search:
+            redirect(f'/{search}')
+        redirect('/')
+    table = get_info(key=search)
+    return render_template('search_page.html', title='Поиск', table=table, search=search,
+                               count_search=len(table["apps"]))
+
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home_page.html', title='WinApp', table=get_info())
+    if request.method == 'GET':
+        return render_template('home_page.html', title='WinApp', table=get_info())
+    elif request.method == 'POST':
+        search = request.form.get('search')
+        if search:
+            return redirect(f'/search/{search}')
+        return redirect('/')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    search = request.form.get('search')
+    if search:
+        return redirect(f'/search/{search}')
     if form.validate_on_submit():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.email == form.email.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
-            return redirect("../home")
+            return redirect("..")
         return render_template('login.html',
                                message="Неправильный логин или пароль",
                                form=form)
@@ -58,6 +102,9 @@ def login():
 @app.route('/register', methods=['GET', 'POST'])
 def reqister():
     form = RegisterForm()
+    search = request.form.get('search')
+    if search:
+        return redirect(f'/search/{search}')
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
             return render_template('register.html', title='Регистрация',
@@ -68,18 +115,23 @@ def reqister():
             return render_template('register.html', title='Регистрация',
                                    form=form,
                                    message="Такой пользователь уже есть")
-        user = User(name=form.name.data,
-                    email=form.email.data,
-                    surname=form.surname.data)
+        user = User(
+            name=form.name.data,
+            surname=form.surname.data,
+            email=form.email.data,
+        )
         user.set_password(form.password.data)
         db_sess.add(user)
         db_sess.commit()
-        return redirect('../login')
+        return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
 
 
-@app.route('/app/<app_name>')
+@app.route('/app/<app_name>', methods=['GET', 'POST'])
 def app_page(app_name):
+    search = request.form.get('search')
+    if search:
+        return redirect(f'../search/{search}')
     table = get_info()
     for i in table['apps']:
         if i['name'] == app_name:
@@ -89,23 +141,11 @@ def app_page(app_name):
     return render_template('app_page.html', title=app_name, name=app_name, table=table)
 
 
-@app.route('/addapp/<app_name>')
-def add_app(app_name):
-    os.mkdir(f'static/apps/{app_name}')
-    app = App(name_app=app_name,
-              description='Нелинейный видеоредактор с открытым исходным кодом на основе Framework MLT и KDE.',
-              company='KDE',
-              page_company='https://kde.org/',
-              link='https://download.kde.org/stable/kdenlive/21.12/windows/kdenlive-21.12.3.exe',
-              icon=f'static/apps/{app_name}/icon',
-              user_id=1
-              )
-    db_sess = db_session.create_session()
-    db_sess.add(app)
-    db_sess.commit()
-    return render_template('base.html')
-
-
 if __name__ == '__main__':
-    db_session.global_init("db/users.db")
-    app.run(port=8080, host='127.0.0.1', debug=True)
+    # answer = input('Вы хотите добавить приложение?(Y/n) ')
+    db_session.global_init("db/users_apps.db")
+    # if answer.lower() == 'y' or answer.lower() == 'yes':
+    #     add_app()
+    # чтобы не запускать режим добавления приложений нужно закомментировать начальную и верхние 2 строчки
+    # OBSStudio не трогайте
+    app.run(port=8080, host='127.0.0.1', debug=False)
